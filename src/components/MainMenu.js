a7ae28b93ff83994b2650e01ef13688d068c31e9
JulianVidal <49546430+JulianVidal@users.noreply.github.com>
2020-05-02 12:27:31 +0300

    Initial commit

---
+++ src/components/MainMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MainMenu.css'
+
+function MainMenu () {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default MainMenu
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainMenu from '../components/MainMenu'
+
+function Main () {
+  return (
+    <div>
+      <MainMenu />
+    </div>
+  )
+}
+
+export default Main
